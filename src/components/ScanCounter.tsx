9a24976c1b3bf38309d9bbc09e84e054254913c0
jedtipudan <jedtipudan@gmail.com>
2026-09-19 15:54:53 +0800

    Module 1: Scaffold Expo TypeScript project

--- src/components/ScanCounter.tsx
+++ src/components/ScanCounter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 interface ScanCounterProps {
   count: number;
